a2d2053085f6245e524526d6c1fa831e5056976b
Arnold-Babu <arnoldbabu2004@gmail.com>
2025-01-10 15:17:01 -0500

    Src added

---
+++ ice/src/client/app/animals/search.js
@@ -0,0 +1,7 @@
+/*
+    Name: Arnold Babu
+    filename: app.js
+    Course: INFT 2202
+    Date: January 10, 2025
+    Description: This is my general style sheet.  Styles that affect all pages should go here.
+*/
