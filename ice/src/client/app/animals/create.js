d5f3c7be723afa2faeba0693c8e7315254091daf
Arnold-Babu <arnoldbabu2004@gmail.com>
2025-01-10 18:34:23 -0500

    Updated Comments

--- ice/src/client/app/animals/create.js
+++ ice/src/client/app/animals/create.js
@@ -3,5 +3,5 @@
     filename: app.js
     Course: INFT 2202
     Date: January 10, 2025
-    Description: This is my animal create sheet.  Styles that affect all pages should go here.
+    Description: This is my animal create sheet.
 */
